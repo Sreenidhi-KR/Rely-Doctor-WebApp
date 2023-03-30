cc18c9ea47b71fd8a384f9113fc46590a893aa58
Prajwal-jm25 <prajwal.jm25@gmail.com>
2023-03-30 15:14:41 +0530

    Added Profile component and made changes in styles

--- client/src/components/profile.component.js
+++ client/src/components/profile.component.js
@@ -1,38 +1,37 @@
 import React, { useState } from "react";
-import { Navigate } from "react-router-dom";
 import AuthService from "../services/auth.service";
 
 function Profile() {
-    const currentUser = AuthService.getCurrentUser();
-    
+    const [img, setImg] = useState("");
     const [doctor,setDoctor] = useState("");
+
     window.onload = () => {
       AuthService.getDoctor(setDoctor);
+      AuthService.getPhoto(setImg);
       }
     console.log(doctor);
 
     return (
       <div>
          <div className="card text-black mb-5" style={{maxWidth: '75rem', height:'37rem', marginLeft:'17%', zIndex:50}}>
-          <div className="card-header" style={{color : 'white', backgroundColor:'black'}}>Profile Details
-          <button type="button" className="btn-close" onClick={() => props.setIsOpen(false)} aria-label="Close" style={{float : 'right', backgroundColor : 'white'}}></button>
+          <div className="card-header" style={{color : 'white', backgroundColor:"rgb(38, 201, 225)", fontWeight:"bold", fontSize:"20px"}}>Profile Details
         </div>
         <div className="card-body">
-        <img alt="" style={{float:'right', borderRadius:10, boxShadow:'10px', width:'550px', height:'520px'}} src={`data:image/png;base64,${props.image}`}></img>
-          <h5 className="card-title">Name</h5>
-          <p className="card-text">{props.user.first_name} {props.user.last_name}</p>
-          <h5 className="card-title">Roll Number</h5>
-          <p className="card-text">{props.user.roll_number}</p>
-          <h5 className="card-title">Domain</h5>
-          <p className="card-text">{props.user.domain.program}</p>
-          <h5 className="card-title">Specialisation</h5>
-          <p className="card-text">{props.user.specialization.name}</p>
-          <h5 className="card-title">Current CGPA</h5>
-          <p className="card-text">{props.user.cgpa}</p>
-          <h5 className="card-title">Email ID</h5>
-          <p className="card-text">{props.user.email}</p>
-          <h5 className="card-title">Graduation Year</h5>
-          <p className="card-text">{props.user.graduation_year}</p>
+        <img alt="" style={{float:'right', borderRadius:15, boxShadow:'20px', width:'440px', height:'480px'}} src={`data:image/png;base64,${img}`}></img>
+          <h5 className="card-title" style={{color:"rgb(38, 201, 225)"}}>Name</h5>
+          <p className="card-text">{doctor.fname} {doctor.lname}</p>
+          <h5 className="card-title" style={{color:"rgb(38, 201, 225)"}}>Date Of Birth</h5>
+          <p className="card-text">{doctor.dob}</p>
+          <h5 className="card-title" style={{color:"rgb(38, 201, 225)"}}>Sex</h5>
+          <p className="card-text">{doctor.sex}</p>
+          <h5 className="card-title" style={{color:"rgb(38, 201, 225)"}}>Specialisation</h5>
+          <p className="card-text">{doctor.specialization}</p>
+          <h5 className="card-title" style={{color:"rgb(38, 201, 225)"}}>Qualification</h5>
+          <p className="card-text">{doctor.qualification}</p>
+          <h5 className="card-title" style={{color:"rgb(38, 201, 225)"}}>Email ID</h5>
+          <p className="card-text">{doctor.email}</p>
+          <h5 className="card-title" style={{color:"rgb(38, 201, 225)"}}>Rating</h5>
+          <p className="card-text">{doctor.rating}</p>
         </div>
         </div>
 
--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -4,6 +4,7 @@ import authHeader from "./auth-header";
 const API_URL = "http://localhost:8080/api/auth/doctor/";
 const urlBase = "http://localhost:8080/api";
 
+
 const config = {
   // headers: {
   //   "ngrok-skip-browser-warning": "true",
@@ -11,6 +12,17 @@ const config = {
   // },
   headers: authHeader()
 };
+const configPhoto=null;
+try{
+  configPhoto = {
+    headers:{
+      "Content-Type":"multipart/form-data",
+      "Authorization":"Bearer "+JSON.parse(localStorage.getItem('doctor')).accessToken
+    }
+  }
+}catch(err){
+  console.log(err)
+}
 
 class AuthService {
   login(username, password) {
@@ -36,11 +48,6 @@ class AuthService {
     return JSON.parse(localStorage.getItem('doctor'));
   }
 
-  // getDoctorDetails = async() => {
-  //   const id=JSON.parse(localStorage.getItem('doctor')).id;
-  //   return await axios.get(`${urlBase}/v1/doctor/getDoctorById/${id}`, config).then(response => response.data);
-  // }
-
   getDoctor = (setDoctor) => {
     const id=JSON.parse(localStorage.getItem('doctor')).id;
     axios
@@ -55,7 +62,19 @@ class AuthService {
       });
     };
 
-  
+    getPhoto = (setImg) => {
+      const id=JSON.parse(localStorage.getItem('doctor')).id;
+      axios
+        .get(`${urlBase}/v1/doctor/getPhotoById/${id}`, configPhoto)
+        .then((json) => {
+          setImg(json.data);
+          console.log(json.data)
+          return json.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      };
 }
 
 export default new AuthService();
